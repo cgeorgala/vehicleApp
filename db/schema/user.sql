072af4ba2592e92b2158520aea79c4d10ce78186
Christina Georgala <christina.georgala@nokia.com>
2021-06-28 02:35:03 +0300

    Modify email NOT being unique in DB

--- db/schema/user.sql
+++ db/schema/user.sql
@@ -18,6 +18,6 @@ CREATE TABLE users(
   CONSTRAINT user_pkey PRIMARY KEY (id),
 
   -- Unique
-  CONSTRAINT user_umail_key UNIQUE (email),
+  -- CONSTRAINT user_umail_key UNIQUE (email),
   CONSTRAINT user_uname_key UNIQUE (username)
 );
